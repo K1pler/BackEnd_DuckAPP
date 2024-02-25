33229d83ac50de561360b5b575f85027cfb93ff0
k1pler <roland1843lol@gmail.com>
2024-02-25 18:06:32 +0100

    https node.js

--- mi-proyecto-backend/server2.js
+++ mi-proyecto-backend/server2.js
@@ -1,20 +1,42 @@
 const express = require('express');
-const app = express();
+const https = require('https');
+const fs = require('fs');
 const path = require('path');
-const port = 3000; // El puerto que prefieras
+const app = express();
 
-// Define la ruta absoluta hacia la carpeta del juego de Unity
+const port = 3000; // Define la variable 'port' que se usará para el servidor HTTPS
+
+// Ruta absoluta hacia la carpeta del juego de Unity
 const unityGamePath = 'C:/Users/rolan/Desktop/DUCK-APP/files/Pong';
 
-// Sirve los archivos estáticos de tu juego de Unity WebGL
+// Cargar clave privada y certificado para HTTPS
+const privateKey = fs.readFileSync(path.join(__dirname, 'server.key'), 'utf8');
+const certificate = fs.readFileSync(path.join(__dirname, 'server.cert'), 'utf8');
+
+// Sirve los archivos estáticos de tu juego de Unity WebGL con Brotli si está disponible
+// Middleware para manejar la codificación Brotli
+app.get('*.*', (req, res, next) => {
+    if (req.url.endsWith('.br')) {
+      res.set('Content-Encoding', 'br');
+      res.set('Content-Type', req.url.endsWith('.js') ? 'application/javascript' : 'text/plain');
+    }
+    next();
+  });
+
 app.use(express.static(unityGamePath));
 
 // Ruta principal que sirve tu juego Unity
 app.get('/', (req, res) => {
-  res.sendFile(path.join(unityGamePath, 'index.html'));
+    res.sendFile(path.join(unityGamePath, 'index.html'));
 });
 
-// Iniciar el servidor
-app.listen(port, () => {
-  console.log(`Servidor escuchando en http://localhost:${port}`);
+// Crear un servidor HTTPS
+const httpsServer = https.createServer({
+  key: privateKey,
+  cert: certificate
+}, app);
+
+// Iniciar el servidor HTTPS
+httpsServer.listen(port, () => {
+    console.log(`Servidor escuchando en https://localhost:${port}`);
 });
